a16b6fbd2f167710ec915886c0f86e394f2c9952
dotte@oregonstate.edu <dotte@oregonstate.edu>
2022-01-31 17:13:14 -0800

    fixed unittest

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,78 +8,95 @@ describe('App', () => {
   });
 });
 
-//Shelter -- can't really test well because postShelters is async function
-var r = Math.floor(Math.random() * 10000);
+//Shelter
+describe('shelters', () => {
+  var r = Math.floor(Math.random() * 10000);
+	it('POST /shelters responds 201', () => {
+		return supertest(app)
+			.post('/shelters')
+			.send({
+        "shelterName": "Shelter 1",
+        "address": r + "1 Chicago, IL",
+        "emailAddress": r + "Shelter12@email.com",
+        "password":"Shelter12345",
+        "phoneNumber": "0000000000",
+        "website":"website.com"
+    })
+			.set('Accept', 'application/json')
+			.expect(201)
+	});
 
-var json = {
-  "shelterName": "Shelter " + r,
-  "address": r + " Dallas, Tx",
-  "emailAddress": r + "Shelter@email.com",
-  "password": "Shelter" + r,
-  "phoneNumber": "0000000000",
-  "website": "www.test.com"
-}
-// describe('shelters 1', () => {
-// 	it('POST /shelters responds 201', () => {
-// 		return supertest(app)
-// 			.post('/shelters')
-// 			.send(json)
-// 			.set('Accept', 'application/json')
-// 			.expect(201)
-// 	});
-//  });
+  it('POST /shelters responds 400 (duplicate data)', () => {
+		return supertest(app)
+			.post('/shelters')
+			.send({
+        "shelterName": "Shelter 1",
+        "address": r + "1 Chicago, IL",
+        "emailAddress": r + "Shelter12@email.com",
+        "password":"Shelter12345",
+        "phoneNumber": "0000000000",
+        "website":"website.com"
+    })
+			.set('Accept', 'application/json')
+			.expect(400, "Duplicate entry")
+	});
 
-// describe('shelters 2', () => {
-// 	it('POST /shelters responds 400 (duplicate data)', () => {
-// 		return supertest(app)
-// 			.post('/shelters')
-// 			.send(json)
-// 			.set('Accept', 'application/json')
-// 			.expect(400, "Duplicate entry")
-// 	});
-// });
+  it('POST /shelters responds 400 (null value)', () => {
+		return supertest(app)
+			.post('/shelters')
+			.send({
+        "shelterName": null,
+        "address": r + "1 Chicago, IL",
+        "emailAddress": r + "Shelter12@email.com",
+        "password":"Shelter12345",
+        "phoneNumber": "0000000000",
+        "website":"website.com"
+    })
+			.set('Accept', 'application/json')
+			.expect(400, "Please provide required input values")
+	});
 
-// describe('shelters 3', () => {
-// 	json["shelterName"] = null;
-// 	it('POST /shelters responds 400 (null value)', () => {
-// 		return supertest(app)
-// 			.post('/shelters')
-// 			.send(json)
-// 			.set('Accept', 'application/json')
-// 			.expect(400, "Please provide required input values")
-// 	});
-// });
-// describe('shelters 4', () => {
-// 	delete json["shelterName"];
-// 	it('POST /shelters responds 400 (missing attribute)', () => {
-// 		return supertest(app)
-// 			.post('/shelters')
-// 			.send(json)
-// 			.set('Accept', 'application/json')
-// 			.expect(400, "Please provide required input attributes")
-// 	});
-// });
-describe('shelters 5', () => {
-  r = Math.floor(Math.random() * 10000);
-  json["address"] = r + " Austin, Tx";
-  json["website"] = null;
+  it('POST /shelters responds 400 (missing attributes)', () => {
+		return supertest(app)
+			.post('/shelters')
+			.send({
+        "address": "1 Austin, TX",
+        "emailAddress": r + "Austin@email.com",
+        "password": r + "Shelter12345",
+        "phoneNumber": "0000000000",
+        "website":"website.com"
+    })
+			.set('Accept', 'application/json')
+			.expect(400, "Please provide required input attributes")
+	});
+
   it('POST /shelters responds 201 (website = null)', () => {
     return supertest(app)
       .post('/shelters')
-      .send(json)
+      .send({
+        "shelterName": "Shelter Tokyo",
+        "address": r + "1234 Tokyo, Japan",
+        "emailAddress": r + "Shelter12@email.com",
+        "password":"Shelter12345",
+        "phoneNumber": "0000000000",
+        "website":null
+    })
       .set('Accept', 'application/json')
       .expect(201)
   });
-});
-// describe('shelters 6', () => {
-// 	r = Math.floor(Math.random() * 10000);
-// 	json["address"] = r + " Houston, Tx";
-// 	delete json["website"];
-// 	it('POST /shelters responds 201 (missing website attribute)', () => {
-// 		return supertest(app)
-// 			.post('/shelters')
-// 			.send(json)
-// 			.set('Accept', 'application/json')
-// 			.expect(201)
-// 	});
-// });
+
+  it('POST /shelters responds 201 (missing website attribute)', () => {
+		return supertest(app)
+			.post('/shelters')
+			.send({
+        "shelterName": "Shelter Portland1",
+        "address": r + "1 Portland, OR",
+        "emailAddress": r + "Shelter12@email.com",
+        "password":"Shelter12345",
+        "phoneNumber": "0000000000",
+    })
+			.set('Accept', 'application/json')
+			.expect(201)
+	});
+ });
+
