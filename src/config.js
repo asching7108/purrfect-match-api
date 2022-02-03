fce772df5c18db2ab08acaaa43a9f6eb11f9c7a0
xHoudek <xhoudek@gmail.com>
2022-02-03 10:08:58 -0600

    PM-13: addressed PR comments

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-	NODE_ENV: process.env.NODE_ENV || 'development'
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  SECRET: 'notASecret' // A secret is required by jsonwebtoken, but is not used by react-jwt
 };
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,40 +3,52 @@ const {
   verifyLoginCredentials,
   updateUserSession
 } = require('../models/usersModel');
+const { SECRET } = require('../config');
+const { inputValidation } = require('../utils/tools');
 
 const login = async (req, res, next) => {
-  const { email, password } = req.body;
 
-  await verifyLoginCredentials(email, password)
-    .then((dbResponse) => {
+  // input validation
+  var attrs = ["email", "password"];
+  var contype = req.headers['content-type'];
+  if (!contype || contype.indexOf('application/json') !== 0) res.sendStatus(415);
+  else if (!inputValidation.hasAllAttrs(req.body, attrs)) res.status(400).send("Please provide required input attributes");
+  else if (inputValidation.includesNullorEmpty(req.body)) res.status(400).send("Please provide required input values");
+  else {
+    const { email, password } = req.body;
 
-      // Verify email and password
-      if (dbResponse.length !== 1) {
-        // There should only ever be one user with the same email and pw
-        res.status(400).send('Bad Request');
-      }
-      const userID = dbResponse[0].UserID;
+    await verifyLoginCredentials(email, password)
+      .then((dbResponse) => {
 
-      // Create unique session ID (aka jwt)
-      const token = jwt.sign(
-        {
-          data: email
-        },
-        'notASecret',
-        { expiresIn: '1h' }
-      );
+        // Verify email and password
+        if (dbResponse.length !== 1) {
+          // There should only ever be one user with the same email and pw
+          res.status(401).send('Unauthorized');
+        } else {
+          const userID = dbResponse[0].UserID;
 
-      // Save to DB
-      saveUserToken(userID, token).then(() => {
-        // Return token
-        res.send({ token: token });
+          // Create unique session ID (aka jwt)
+          const token = jwt.sign(
+            {
+              data: email
+            },
+            SECRET,
+            { expiresIn: '1h' }
+          );
+
+          // Save to DB
+          saveUserToken(userID, token).then(() => {
+            // Return token
+            res.send({ token: token });
+          });
+        }
+      })
+      .catch((e) => {
+        console.log(e.message);
+        res.status(500).send('Server error');
+        next(e);
       });
-    })
-    .catch((e) => {
-      console.log(e.message);
-      res.status(500).send('Server error');
-      next(e);
-    });
+  }
 };
 
 const saveUserToken = async (userID, token) => {
