f1adc7f4bde9761bd32a90e47866532a8af634bd
asching7108 <asching7108@gmail.com>
2022-02-03 21:14:07 -0500

    PM-9 fixed merging issue

--- src/controllers/sheltersController.js
+++ src/controllers/sheltersController.js
@@ -104,32 +104,6 @@ const deleteShelter = async (req, res, next) => {
     });
 };
 
-const postShelters = async (req, res, next) => {
-
-  var attrs = ["address", "emailAddress", "password", "phoneNumber", "shelterName"];
-  var contype = req.headers['content-type'];
-  //check content type
-  if (!contype || contype.indexOf('application/json') !== 0) res.sendStatus(406);
-  //check all attrs are provided (input validation)
-  else if (!inputValidation.hasAllAttrs(req.body, attrs)) res.status(400).send("Please provide required input attributes");
-  //check null values
-  else if (inputValidation.includesNullorEmpty(req.body, ["website"])) res.status(400).send("Please provide required input values");
-  else {
-    await createShelters(req.body)
-      .then((dbResponse) => {
-        res.status(201).type('json').send(dbResponse);
-      })
-      .catch((e) => {
-        console.log(e.message)
-        if (e.message.includes("ER_DUP_ENTRY")) res.status(400).send("Duplicate entry");
-        else
-          res.sendStatus(500);
-        next(e);
-      });
-  }
-
-
-};
 module.exports = {
   getShelters,
   postShelters,
