5dd1380ee2060d589d24c60209c0d9a99116bc0e
xHoudek <xhoudek@gmail.com>
2022-02-08 16:07:50 -0600

    PM-24: undo db config commit (oops)

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   HOST: "localhost",
   PORT: '3306',
-  USER: "houdeka",
-  PASSWORD: "Password!",
-  DB: "purrfect_match_local",
+  USER: "[YOUR_USERNAME]",
+  PASSWORD: "[YOUR_PASSWORD]",
+  DB: "purrfect-match_dev",
   DEV_HOST: "localhost",
   DEV_PORT: '3306',
   DEV_USER: "[YOUR_DEV_USERNAME]",
