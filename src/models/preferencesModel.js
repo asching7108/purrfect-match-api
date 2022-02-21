a7c2df1f4c23321c0265c36923f4faa2d9bb2c70
xHoudek <xhoudek@gmail.com>
2022-02-21 13:48:36 -0600

    PM-53: added create, read all, and two deletes for user preferences

---
+++ src/models/preferencesModel.js
@@ -0,0 +1,74 @@
+const mysql = require('mysql');
+const { Logger } = require("../utils/log4js.js");
+const log = Logger();
+
+const createPreference = async (db, userID, params) => {
+  const sql = 'INSERT INTO UserPreference (UserID, PreferenceType, PreferenceSelection) '
+    + 'VALUES (?, ?, ?)';
+
+  const vals = [
+    userID,
+    params.type,
+    params.selection,
+  ];
+  log.debug("Running createPreference sql = " + mysql.format(sql, vals));
+  return new Promise((resolve, reject) => {
+    db.query(sql, vals, (err, res, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+const getAllUserPreferences = async (db, userID) => {
+  const sql = 'SELECT * FROM UserPreference WHERE UserID = ?';
+  const vals = [userID];
+  log.debug("Running getAllUserPreferences sql = " + mysql.format(sql, vals));
+  return new Promise((resolve, reject) => {
+    db.query(sql, vals, (err, res, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+const deletePreferenceByID = async (db, preferenceID) => {
+  const sql = 'DELETE FROM UserPreference WHERE UserPreferenceID = ?';
+  log.debug("Running deletePreferenceByID sql = " + mysql.format(sql, preferenceID));
+  return new Promise((resolve, reject) => {
+    db.query(sql, preferenceID, (err, res, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+const deletePreferencesByUserID = async (db, userID) => {
+  const sql = 'DELETE FROM UserPreference WHERE UserID = ?';
+  log.debug("Running deletePreferencesByUserID sql = " + mysql.format(sql, userID));
+  return new Promise((resolve, reject) => {
+    db.query(sql, userID, (err, res, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+module.exports = {
+  createPreference,
+  getAllUserPreferences,
+  deletePreferenceByID,
+  deletePreferencesByUserID
+}
